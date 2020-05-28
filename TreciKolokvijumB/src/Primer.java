d6c3ecbf57d51bdcfd69f86b4c74e057b6c7c27b
Karolina Bicok <bicokkarolina@gmail.com>
2020-05-29 00:17:37 +0200

    Bez iceg

--- TreciKolokvijumB/src/Primer.java
+++ TreciKolokvijumB/src/Primer.java
@@ -1,13 +1,10 @@
 	
 	public int oddOrPos(int[] x) {
 		int count = 0;
-<<<<<<< HEAD
+
 		for (int i = 0; i < x.length +1; i++) {
 			if (x[i] % 2 == 1 && x[i] > 0) {
-=======
-		for (int i = 0; i <= x.length - 1; i++) {
-			if (x[i] % 2 == 1 || x[i] > 0) {
->>>>>>> karolina-bicok
+
 				count++;
 			}
 		}
